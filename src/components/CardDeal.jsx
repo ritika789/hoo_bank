dc9633e9dc218fb584444aa2c17ada5f7655bc94
ritika <ritikakaur789@gmail.com>
2023-04-17 12:17:17 +0530

    first commit

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,28 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      Step 1: Choose your Credit Card
+      Step 2: Apply for your card
+      Step 3: Activate your card 
+
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
